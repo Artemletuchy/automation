99e121c46b9e39cda7d914aa11fd2258f5d0bf30
Artem_Letuchyi <artletuchy@gmail.com>
2025-02-26 00:48:00 +0200

    lesson 3

---
+++ Lessons/test.js
@@ -0,0 +1,22 @@
+// console.log("Привет, JavaScript в Node.js! 🚀");
+
+let Name = "Jone"; // String
+let age = 30; // Nubmer
+let isDeveloper = true; //Boolean
+let empty = null; // Null
+let undef = undefined // undefined
+let symbol = Symbol('id') // symbol
+let Bint = 12345678987654n //Bint
+
+console.log(typeof Name, Name);
+console.log(typeof age, age);
+console.log(typeof isDeveloper, isDeveloper);
+console.log(typeof empty, empty);
+console.log(typeof undef, undef);
+console.log(typeof symbol, symbol);
+console.log(typeof Bint, Bint);
+
+
+let obj = { name:  "test"} //обект
+let obр = ['Test'] // массив
+
