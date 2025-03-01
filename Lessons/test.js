246c8cc30ca21482d5c24ac1fc9d30ab288c4140
Artem_Letuchyi <artletuchy@gmail.com>
2025-03-01 12:46:33 +0200

    Lesson4 homework

--- Lessons/test.js
+++ Lessons/test.js
@@ -1,22 +1,70 @@
 // console.log("Привет, JavaScript в Node.js! 🚀");
 
-let Name = "Jone"; // String
-let age = 30; // Nubmer
-let isDeveloper = true; //Boolean
-let empty = null; // Null
-let undef = undefined // undefined
-let symbol = Symbol('id') // symbol
-let Bint = 12345678987654n //Bint
+// let Name = "Jone"; // String
+// let age = 30; // Nubmer
+// let isDeveloper = true; //Boolean
+// let empty = null; // Null
+// let undef = undefined // undefined
+// let symbol = Symbol('id') // symbol
+// let Bint = 12345678987654n //Bint
 
-console.log(typeof Name, Name);
-console.log(typeof age, age);
-console.log(typeof isDeveloper, isDeveloper);
-console.log(typeof empty, empty);
-console.log(typeof undef, undef);
-console.log(typeof symbol, symbol);
-console.log(typeof Bint, Bint);
+// console.log(typeof Name, Name);
+// console.log(typeof age, age);
+// console.log(typeof isDeveloper, isDeveloper);
+// console.log(typeof empty, empty);
+// console.log(typeof undef, undef);
+// console.log(typeof symbol, symbol);
+// console.log(typeof Bint, Bint);
 
 
-let obj = { name:  "test"} //обект
-let obр = ['Test'] // массив 
+// let obj = { name:  "test"} //обект
+// let obр = ['Test'] // массив 
 
+// let age = 17;
+
+// function canDrive(age){
+  
+//   if (age >= 18){
+//     return true;
+//   } else {
+//     return false
+//   }
+// }
+
+// console.log(CanDrive(age))
+
+// let username = 4
+
+// function greet(name){
+//   if (name === "" || name === undefined){
+//     return "Hi, my friend"
+//   } else {
+//     return "Hi, " + name + " !"
+//   }
+// }
+
+// console.log(greet(username))
+
+// let N = 6
+
+// function isEven(number){
+//   if(typeof number !== "number"){                            //"number" в ковычках потому что typeof всегда возвращает строку а не тип данных 
+//     return "Error it is not number"
+//   }
+
+//   if (number % 2 === 0){
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+// console.log(isEven(N))
+
+// let nameA = "Artem"
+// let lastName = "Letuchy"
+
+// console.log(`Log: ${nameA} ${lastName}`)
+
+// console.log('Hello' === 'Hello' ? 1 : 0)
+
+// console.log("age" === `${radius}` ? "Yes u best": "No it is not true") // он сравнивает и выдает да или нет неважно что там будет написано 
---
+++ jsBasics/task1.js
@@ -0,0 +1,13 @@
+let lastName = "Gvozd";
+let age = 3;
+let go = true;
+let empty = null;
+let undef = undefined;
+
+console.log(lastName)
+console.log(age)
+console.log(go)
+console.log(empty)
+console.log(undef)
+
+
