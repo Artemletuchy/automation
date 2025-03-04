83ec38bf052c2df46741f0c1b7bf4f4c0d648ff2
Artem_Letuchyi <artletuchy@gmail.com>
2025-03-05 01:52:31 +0200

    less5

--- Lessons5/task1.js
+++ Lessons5/task1.js
@@ -16,35 +16,3 @@ else if (averageGrade >= 91 && averageGrade <= 100 ){
   console.log("Perfect")
 }
 else console.log("Anyone")
-
-
-
-switch(true){
-
-
-  case averageGrade < 60:
-    console.log("Unsatisfactorily")
-  break
-
-  case averageGrade >= 60 && averageGrade <= 70:
-    console.log("Satisfactorily")
-  break
-
-  case averageGrade >= 71 && averageGrade <= 80 :
-    console.log("Done")
-
-  break
-
-  case averageGrade >= 81 && averageGrade <= 90 :
-    console.log("Very well")
-  
-  break
-
-  case averageGrade >= 91 && averageGrade <= 100 :
-    console.log("Perfect")
-  
-  break
-
-  default : console.log("Anyone")
-
-}
