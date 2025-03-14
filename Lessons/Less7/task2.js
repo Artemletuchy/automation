05687d30b99e4660dbbbb3c03407e0a57d579fcd
Artem_Letuchyi <artletuchy@gmail.com>
2025-03-15 00:28:24 +0200

    lesson7

---
+++ Lessons/Less7/task2.js
@@ -0,0 +1,24 @@
+function task(num) {
+  if (num <= 0) { 
+    return
+  }
+  
+  console.log(num)
+  
+  task(num - 1)
+}
+
+task(5)
+
+console.log("____________________")
+
+
+function task2(num) {
+
+  for (let i = num; i > 0; i--) {
+    console.log(i);
+  }
+}
+
+task2(10);
+
