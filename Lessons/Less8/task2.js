50dbe3e879c169280a817f7905aec42ce00e4e55
Artem_Letuchyi <artletuchy@gmail.com>
2025-03-20 23:26:22 +0200

    lesson8

---
+++ Lessons/Less8/task2.js
@@ -0,0 +1,9 @@
+const massiv = [1, 2, 3, 4, 5]
+
+ const mutatedNumbers = massiv.map(function BBB(num,index){
+   return num * index
+ })
+
+
+
+console.log(mutatedNumbers)
---
+++ Lessons/Less8/task6.js
@@ -0,0 +1,10 @@
+const numbersList = [1,10,14,2,4,5,43,34]
+
+const copiedArray = numbersList.slice()
+
+const SS = copiedArray.sort(function UUU(a,b){
+  return a - b
+
+})
+console.log(SS)
+
