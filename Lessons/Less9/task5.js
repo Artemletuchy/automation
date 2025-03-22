a15fe1d27f149f6312ae4f6271722133c02f6ca2
Artem_Letuchyi <artletuchy@gmail.com>
2025-03-22 12:27:56 +0200

    hot fix

--- Lessons/Less9/task5.js
+++ Lessons/Less9/task5.js
@@ -4,6 +4,11 @@ const users =[
   {name : "valiiii", email : "valiii@gmail.com", age : 14}
 ]
 
+for(let user of users){
+  console.log(user)
+}
+
 for (let {name,email,age} of users){
   console.log(`My name is ${name}, if u have notif for me ${email}, today my birthday and my age ${age}`)
 }
+
